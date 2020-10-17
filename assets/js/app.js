08116c2de4a31cb72c158656f7722f68597da5f6
sigma <sigma@localhost.localdomain>
2020-10-17 13:24:38 +0530

    PWA notifictaion added for contact me

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,29 @@ function doShare() {
     }
 }
 
+if (Notification.permission == 'granted') {
+    var firstTime = localStorage.getItem("firsttime");
+    if(firstTime != "no")
+    navigator.serviceWorker.getRegistration().then((reg) => {
+        var options = {
+            body: 'Feel Free to Contact Me',
+            icon: '/favicon.png',
+            vibrate: [100, 50, 100],
+            data: {
+                dateOfArrival: Date.now(),
+                primaryKey: 1
+            },
+            // actions: [
+            //     { action: 'explore', title: 'Yes', icon: '/favicon.png'},
+            //     { action: 'close', title: 'No', icon: '/favicon.png'}
+            // ]
+        };
+        reg.showNotification('', options);
+        localStorage.setItem("firsttime","no");
+    });
+}
+
+
 if(!navigator.canShare) {
     document.getElementById("share-btn").style.display = "none";
 }
